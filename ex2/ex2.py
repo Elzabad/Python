40ef8f91f07cfa879cec0fb7733f2c67bca7bf3d
Elzabad <elzabad.kennedy>
2021-11-16 20:07:51 -0500

    Finished ex2-4

---
+++ ex2/ex2.py
@@ -0,0 +1,9 @@
+# A comment, this is so  you can read your prgram later.
+# Anything after a # is ignrded by python.
+
+print("I could have code like this.") # and the cmment after is ignored
+
+# You can also use a coomment to "disable" or comment out a piece of code:
+# ptiner("Thi wont run.")
+
+print("This will run.")
